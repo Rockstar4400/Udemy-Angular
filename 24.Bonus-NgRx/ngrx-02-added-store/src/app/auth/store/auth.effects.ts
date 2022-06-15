5cdb870984e9c478a8221158aa9ba48e13f35551
Alberto Del Angel <rockstar4400@gmail.com>
2022-06-14 23:38:32 -0500

    Effects Angular

--- 24.Bonus-NgRx/ngrx-02-added-store/src/app/auth/store/auth.effects.ts
+++ 24.Bonus-NgRx/ngrx-02-added-store/src/app/auth/store/auth.effects.ts
@@ -1,26 +1,27 @@
-import { HttpClient } from '@angular/common/http';
-import { Actions, ofType } from '@ngrx/effects';
-import { switchMap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from "@angular/common/http";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { of } from "rxjs";
+import { catchError, switchMap, map } from "rxjs/operators";
+import { environment } from "src/environments/environment";
 
-import * as AuthActions from './auth.actions';
+import * as AuthActions from "./auth.actions";
 
 export interface AuthResponseData {
-    kind: string;
-    idToken: string;
-    email: string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
-    registered?: boolean;
-  }
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
 
 export class AuthEffects {
-    authLogin = this.actions$.pipe(
-        ofType(AuthActions.LOGIN_START),
-        switchMap((authData: AuthActions.LoginStart) => {
-            return this.http
-      .post<AuthResponseData>(
+  @Effect()
+  authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http.post<AuthResponseData>(
         "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" +
           environment.firebaseAPIKey,
         {
@@ -28,9 +29,15 @@ export class AuthEffects {
           password: authData.payload.password,
           returnSecureToken: true,
         }
-      )
-        })
-    );
+      ).pipe(catchError(error =>{
+        return of();
+      }), 
+      map(resData =>{
+        return of();
+      })
+      );
+    }),
+  );
 
-    constructor(private actions$: Actions, private http: HttpClient) {}
+  constructor(private actions$: Actions, private http: HttpClient) {}
 }
