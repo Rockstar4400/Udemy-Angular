96eaa1f0213d80bd304c090d2ac109a147472cb7
Alberto Del Angel <rockstar4400@gmail.com>
2022-06-14 22:04:13 -0500

    Change somethings

--- 24.Bonus-NgRx/ngrx-02-added-store/src/app/auth/store/auth.actions.ts
+++ 24.Bonus-NgRx/ngrx-02-added-store/src/app/auth/store/auth.actions.ts
@@ -1,12 +1,12 @@
 import { Action } from "@ngrx/store";
 
 // export const LOGIN_START = '[Auth] Login Start';
-// export const LOGIN = "[Auth] Login";
-// export const LOGOUT = "[Auth] Login";
+export const LOGIN = "[Auth] Login";
+export const LOGOUT = "[Auth] Login";
 
-export const LOGIN = 'LOGIN';
+// export const LOGIN = 'LOGIN';
 export const LOGINOUT = 'LOGINOUT';
-//export const LOGIN_START = '[Auth] Login Start';
+export const LOGIN_START = '[Auth] Login Start';
 
 export class Login implements Action {
   readonly type = LOGIN;
@@ -23,6 +23,12 @@ export class Login implements Action {
 
 export class Logout implements Action {
     readonly type = LOGINOUT;
+}
+
+export class LoginStart implements Action {
+  readonly type = LOGIN_START;
+
+  constructor(public payload: {email:string, password: string}){}
 }
 
 export type AuthActions = Login | Logout;
--- 24.Bonus-NgRx/ngrx-02-added-store/src/app/auth/store/auth.effects.ts
+++ 24.Bonus-NgRx/ngrx-02-added-store/src/app/auth/store/auth.effects.ts
@@ -1,11 +1,36 @@
-// import { Actions, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Actions, ofType } from '@ngrx/effects';
+import { switchMap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
-// import * as AuthActions from './auth.actions';
+import * as AuthActions from './auth.actions';
 
-// export class AuthEffects {
-//     authLogin = this.actions$.pipe(
-//         ofType(AuthActions.LOGIN_START)
-//     );
+export interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
+  }
 
-//     constructor(private actions$: Actions) {}
-// }
+export class AuthEffects {
+    authLogin = this.actions$.pipe(
+        ofType(AuthActions.LOGIN_START),
+        switchMap((authData: AuthActions.LoginStart) => {
+            return this.http
+      .post<AuthResponseData>(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" +
+          environment.firebaseAPIKey,
+        {
+          email: authData.payload.email,
+          password: authData.payload.password,
+          returnSecureToken: true,
+        }
+      )
+        })
+    );
+
+    constructor(private actions$: Actions, private http: HttpClient) {}
+}
